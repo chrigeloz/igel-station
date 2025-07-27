1db0f767c536038b847354820c34431984546cc2
chrigeloz <66828107+chrigeloz@users.noreply.github.com>
2025-07-27 08:20:57 +0200

    Update fieldmap_finder.php

--- fieldmap_finder.php
+++ fieldmap_finder.php
@@ -1,1 +1,11 @@
-
+// Map of database fields to their corresponding human-readable labels for the form
+$fieldMap = [
+    'surname'  => 'Surname',
+    'name'     => 'Name',
+    'phone'    => 'Phone',
+    'email'    => 'Email',
+    'street'   => 'Street',
+    'postcode' => 'Postcode',
+    'suburb'   => 'Suburb',
+    'notes'    => 'Notes'
+];
